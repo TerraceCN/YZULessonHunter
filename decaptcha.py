e6622ec7d763b24cee87a2a983e8af08471074e4
TerraceCN <zky3213@gmail.com>
2021-09-01 11:24:50 +0800

    替换为本地模型

--- decaptcha.py
+++ decaptcha.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
-import json
-from base64 import b64encode
+from io import BytesIO
 
-import httpx
+import numpy as np
+from PIL import Image
+from onnxruntime import InferenceSession
 
-API_URL = 'http://20.89.45.35:8000/decaptcha'
-ERROR_MSG = {
-    -1: '验证码大小错误',
-    -2: '验证码识别API出现错误'
-}
+characters = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789abdefghjmnqrtwxy"
 
-
-class DecaptchaRequestException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
-        self.msg = msg
+model = InferenceSession('urp.onnx')
 
 
 def decaptcha(content):
-    try:
-        resp = httpx.post(API_URL, json={'content': b64encode(content).decode('ascii')})
-    except httpx.HTTPError:
-        raise DecaptchaRequestException('请求验证码识别API时发生错误')
-
-    try:
-        data = resp.json()
-    except json.JSONDecodeError:
-        raise DecaptchaRequestException('验证码识别API返回了错误的数据')
-
-    if data['code'] == 0:
-        return data['result']
-    else:
-        raise DecaptchaRequestException(ERROR_MSG[data['code']])
+    captcha = np.array([np.array(Image.open(BytesIO(content)))]).astype(np.float32)
+    result = model.run([], {'captcha': captcha})
+    y = np.argmax(np.array(result), axis=2)[:,0]
+    return "".join([characters[x] for x in y])
